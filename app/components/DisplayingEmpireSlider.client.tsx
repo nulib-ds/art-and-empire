0a9389049b7473abe36b306c3e2164bbbaf6d4e3
Mat Jordan <mat@northwestern.edu>
2026-04-06 14:16:51 -0400

    Update image pathing.

--- app/components/DisplayingEmpireSlider.client.tsx
+++ app/components/DisplayingEmpireSlider.client.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import {withBasePath} from "@canopy-iiif/app/base-path";
 import {FC, useMemo} from "react";
 import {Swiper, SwiperSlide} from "swiper/react";
 import {A11y, Navigation, Pagination} from "swiper/modules";
@@ -13,12 +14,14 @@ export type DisplayingEmpireSliderProps = {
   heading?: string;
 };
 
-const defaultSlides: DisplayingEmpireSlide[] = Array.from(
+const INSTALL_IMAGE_PATHS = Array.from(
   {length: 9},
-  (_, idx) => ({
-    src: `/displaying-empire/install-${idx + 1}.jpeg`,
-  }),
+  (_, idx) => `/displaying-empire/install-${idx + 1}.jpeg`,
 );
+
+const defaultSlides: DisplayingEmpireSlide[] = INSTALL_IMAGE_PATHS.map((src) => ({
+  src,
+}));
 
 const DisplayingEmpireSlider: FC<DisplayingEmpireSliderProps> = ({
   slides,
@@ -28,9 +31,19 @@ const DisplayingEmpireSlider: FC<DisplayingEmpireSliderProps> = ({
     () => (slides && slides.length ? slides : defaultSlides),
     [slides],
   );
+  const resolvedSlides = useMemo(
+    () => mergedSlides.map((slide) => ({
+      ...slide,
+      src: withBasePath(slide.src),
+    })),
+    [mergedSlides],
+  );
 
   return (
     <section className="displaying-empire-slider">
+      {heading ? (
+        <h3 className="displaying-empire-slider__heading">{heading}</h3>
+      ) : null}
       <Swiper
         modules={[Navigation, Pagination, A11y]}
         slidesPerView={1.05}
@@ -45,7 +58,7 @@ const DisplayingEmpireSlider: FC<DisplayingEmpireSliderProps> = ({
         loop={mergedSlides.length > 1}
         className="displaying-empire-slider__swiper"
       >
-        {mergedSlides.map((slide) => (
+        {resolvedSlides.map((slide) => (
           <SwiperSlide
             key={slide.src}
             className="displaying-empire-slider__slide"
@@ -56,9 +69,6 @@ const DisplayingEmpireSlider: FC<DisplayingEmpireSliderProps> = ({
           </SwiperSlide>
         ))}
       </Swiper>
-      {heading ? (
-        <h3 className="displaying-empire-slider__heading">{heading}</h3>
-      ) : null}
     </section>
   );
 };
